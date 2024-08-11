8f0442a7ec8b559382ee3a20a706fd8d392d8a44
Lautenschlager <tai.romani@hotmail.com>
2024-08-11 15:48:40 -0300

    implement bloodweb level control

--- src/core/BloodwebHandler.py
+++ src/core/BloodwebHandler.py
@@ -1,5 +1,7 @@
 import math
 import pyautogui
+import pytesseract
+import re
 from time import sleep
 
 from .ImageMatcher import ImageMatcher
@@ -11,6 +13,12 @@ from utils.logger import logger
 
 class BloodwebHandler:
 	_initialized = False
+
+	RE_BLOODWEB_LEVEL = re.compile(r'BLOODWEB LEVEL (\d+)', re.IGNORECASE)
+
+	strategy_lambda = None
+
+	average_distance_between_two_nodes = None
 
 	grind_strategy = {
 		GRIND_STRATEGY.PRIORITY_THEN_CLOSER_TO_CENTER.value: lambda match, distance_fn: (
@@ -31,10 +39,6 @@ class BloodwebHandler:
 			-match.resource.priority
 		)
 	}
-
-	strategy_lambda = None
-
-	average_distance_between_two_nodes = None
 
 	@classmethod
 	def initialize(cls):
@@ -70,29 +74,46 @@ class BloodwebHandler:
 			height=40
 		)
 
+		self.captured_bloodweb_level = None
+		# Current level included; Used for levels [1,11]\{10}
+		self.skip_x_bloodweb_levels = 0
+		# Current level is not included; Used for level transitions 9>10, 10>11, 11>12, 49>50, 50>1
+		self.get_bloodweb_level_after_x_levels = 0
+
 	def grind_once(self, iteration):
 		result_directory = create_directory(
 			logger.get_result_folder(),
 			iteration
 		)
 
-		(screenshot_path, screenshot) = self.region_bloodweb.take_screenshot(result_directory)
+		(screenshot_path, _) = self.region_bloodweb.take_screenshot(result_directory)
+		#screenshot_path = '.\\..\\result\\00_TEST_2024-08-11T02-45-02.877825\\1\\bloodweb.png'
 
 		matched_bloodweb_nodes = ImageMatcher(
 			image_source=screenshot_path,
 			result_directory=result_directory
 		).match_with_all_resources()
 
+		if self.skip_x_bloodweb_levels > 0:
+			self.click_next_bloodweb(skip=True)
+			return
+
 		self._set_node_locations(matched_bloodweb_nodes)
 
 		self.click_all_nodes()
 
 	def grind(self):
+		logger.log('\n')
+
 		iteration = 0
+
+		# set strategy based on initial level
+		self.get_bloodweb_level()
 
 		while True:
 			iteration += 1
 
+			logger.log('\n')
 			self.grind_once(iteration)
 
 	def click_all_nodes(self):
@@ -119,23 +140,53 @@ class BloodwebHandler:
 		self._click(click_x, click_y)
 
 		delay_until_next_node = (
-			self.region_bloodweb.get_distance_from_center(
-				node.center_x,
-				node.center_y
-			) / self.average_distance_between_two_nodes
+			self.region_bloodweb.get_distance_from_center(click_x, click_y)
+			/ self.average_distance_between_two_nodes
 		) * 0.500
 
 		# gives enough time for the animations to end
 		sleep(delay_until_next_node)
 
-	def click_next_bloodweb(self):
-		logger.log(f'\t=> Moving to the next bloodweb')
+	def click_next_bloodweb(self, skip=False, level_up=False):
+		logger.log(
+			'\t=> Leveling up to the next prestige level'
+			if level_up else
+			f'\t=> {"Skipping" if skip else "Moving"} to the next bloodweb'
+		)
 
 		(center_x, center_y) = self.region_bloodweb.get_center_as_region()
 
 		self._click(center_x, center_y)
 
+		if self.captured_bloodweb_level == 50 and level_up is False:
+			sleep(3.5)
+			# clicks twice in the center, one to grind, one to level_up
+			self.click_next_bloodweb(level_up=True)
+			return
+
 		sleep(4.5)
+		self._check_bloodweb_level_metadata()
+
+
+	def get_bloodweb_level(self):
+		logger.log('\n')
+
+		captured = False
+		while not captured:
+			try:
+				(_, screenshot) = self.region_level.take_screenshot()
+				captured_text = pytesseract.image_to_string(screenshot)
+				captured_level = int(self.RE_BLOODWEB_LEVEL.search(captured_text).group(1))
+
+				captured = True
+			except:
+				# if any error ocurred, especially on captured_level, then try again
+				pass
+
+		logger.log(f'\t=> Identified current bloodweb level: {captured_level}')
+
+		self.captured_bloodweb_level = captured_level
+		self._set_bloodweb_level_metadata()
 
 	def _set_node_locations(self, nodes):
 		nodes.sort(key=lambda match:
@@ -156,3 +207,45 @@ class BloodwebHandler:
 		# unselect anything that might be selected in the bloodweb (popups)
 		pyautogui.moveTo(self.region_bloodweb.x, self.region_bloodweb.y)
 		pyautogui.click()
+
+	def _check_bloodweb_level_metadata(self):
+		if self.get_bloodweb_level_after_x_levels <= 0:
+			self.get_bloodweb_level()
+			self._set_bloodweb_level_metadata()
+		else:
+			self.skip_x_bloodweb_levels -= 1
+			self.get_bloodweb_level_after_x_levels -= 1
+
+	def _set_bloodweb_level_metadata(self):
+		captured_bloodweb_level = self.captured_bloodweb_level
+
+		self.get_bloodweb_level_after_x_levels = 0
+		self.skip_x_bloodweb_levels = 0
+
+		if captured_bloodweb_level >= 12:
+			# covers level transition: 49>50
+
+			# checks that after grinding lvl 49, the next lvl is 50
+			# lvl 50 requires clicking the center twice (grind -> level up)
+			self.get_bloodweb_level_after_x_levels = 49 - captured_bloodweb_level
+
+		elif captured_bloodweb_level == 11:
+			# covers level grinding: 11
+			# covers level transition: 11>12
+
+			# skips lvl 11 (no entity)
+			self.skip_x_bloodweb_levels = 1
+			# checks that after grinding lvl 11, the next lvl is 12
+			self.get_bloodweb_level_after_x_levels = 0
+
+		elif captured_bloodweb_level <= 10:
+			# covers level grinding: 1, 2, 3, 4, 5, 6, 7, 8, 9
+			# covers level transitions: 1>2, 2>3, 3>4, 4>5, 5>6, 6>7, 7>8, 8>9, 9>10
+
+			# skips lvl 1, 2, 3, 4, 5, 6, 7, 8, 9 (no entity)
+			# zeroes out on lvl 10, so it grinds correctly
+			self.skip_x_bloodweb_levels = 10 - captured_bloodweb_level
+			# checks that after grinding lvl 9, the next lvl is 10
+			self.get_bloodweb_level_after_x_levels = self.skip_x_bloodweb_levels - 1 # (10-9)=1-1=0
+
+
