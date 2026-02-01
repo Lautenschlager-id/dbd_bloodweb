4e8d8f3f4db249cb6dcbcbea86b94079ebfc706f
Tai <tai.romani@hotmail.com>
2026-01-31 22:00:37 -0300

    add st chapter 2 stuff

--- src/config/ConfigLoaderSettings.py
+++ src/config/ConfigLoaderSettings.py
@@ -7,72 +7,72 @@ class ConfigLoaderSettings(ConfigLoader):
 	@property
 	def data_schema(self):
 		return {
-		    'type': 'object',
-		    'properties': {
-		    	'parameters': {
-		    		'type': 'array',
-		    		'default': []
-		    	},
-		        'default_match_and_grind_threshold': {
-		            'type': 'number',
-		            'minimum': 0,
-		            'maximum': 1,
-		            'default': 0.75
-		        },
-		        'default_match_and_skip_threshold': {
-		            'type': 'number',
-		            'minimum': 0,
-		            'maximum': 1,
-		            # it's best if it's > default_match_and_grind_threshold
-		            'default': 0.78
-		        },
-		        'grind_strategy': {
-		            'type': 'string',
-		            'enum': list(GRIND_STRATEGY.values()),
-		            'default': GRIND_STRATEGY.DEFAULT.value
-		        },
-		        'use_bloodweb_level_controller': {
-		            'type': 'boolean',
-		            'default': True
-		        },
-		        'disable_addon_resources': {
-		            'type': 'boolean',
-		            'default': False
-		        },
-		        'disable_item_resources': {
-		            'type': 'boolean',
-		            'default': False
-		        },
-		        'disable_offering_resources': {
-		            'type': 'boolean',
-		            'default': False
-		        },
-		        'disable_perk_resources': {
-		            'type': 'boolean',
-		            'default': True
-		        },
-				'disable_box_resources': {
-		            'type': 'boolean',
-		            'default': True
-		        },
-		        'log_skipped_matches': {
-		            'type': 'boolean',
-		            'default': False
-		        },
-				'screen_width': {
-		            'type': 'number',
-		            'default': 1920
-		        },
-				'use_bulk_spend': {
-					'type': 'boolean',
-					'default': True,
-				},
-				'bulk_spend_bloodweb_level_10': {
-					'type': 'boolean',
-					'default': True
-				}
-		    },
-		    'additionalProperties': False
+      'type': 'object',
+      'properties': {
+        'parameters': {
+          'type': 'array',
+          'default': []
+        },
+        'default_match_and_grind_threshold': {
+          'type': 'number',
+          'minimum': 0,
+          'maximum': 1,
+          'default': 0.75
+        },
+        'default_match_and_skip_threshold': {
+          'type': 'number',
+          'minimum': 0,
+          'maximum': 1,
+          # it's best if it's > default_match_and_grind_threshold
+          'default': 0.78
+        },
+        'grind_strategy': {
+          'type': 'string',
+          'enum': list(GRIND_STRATEGY.values()),
+          'default': GRIND_STRATEGY.DEFAULT.value
+        },
+        'use_bloodweb_level_controller': {
+          'type': 'boolean',
+          'default': True
+        },
+        'disable_addon_resources': {
+          'type': 'boolean',
+          'default': False
+        },
+        'disable_item_resources': {
+          'type': 'boolean',
+          'default': False
+        },
+        'disable_offering_resources': {
+          'type': 'boolean',
+          'default': False
+        },
+        'disable_perk_resources': {
+          'type': 'boolean',
+          'default': True
+        },
+        'disable_box_resources': {
+          'type': 'boolean',
+          'default': True
+        },
+        'log_skipped_matches': {
+          'type': 'boolean',
+          'default': False
+        },
+        'screen_width': {
+          'type': 'number',
+          'default': 1920
+        },
+        'use_bulk_spend': {
+          'type': 'boolean',
+          'default': True,
+        },
+        'bulk_spend_bloodweb_level_10': {
+          'type': 'boolean',
+          'default': True
+        }
+      },
+      'additionalProperties': False
 		}
 
 	@property
